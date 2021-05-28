09fe209052ffecea98293c768bda82a85c4d0cad
sudo-Terry <76080411+sudo-Terry@users.noreply.github.com>
2021-05-28 17:16:30 +0900

    fading navbar

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,13 @@
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import styled from "styled-components";
+import {Link} from "react-router-dom";
 
 export const Button = styled(Link)`
   /* border-radius: 50px; */
   white-space: nowrap;
-  background: ${({primary}) => (primary ? '#000d1a' : '#cd853f')};
-  padding: ${({big}) => (big ? '16px 40px' : '14px 24px')};
-  color: ${({primary}) => (primary ? '#fff' : '#000d1a')};
-  font-size: ${({big}) => (big ? '20px' : '14px')};
+  background: ${({primary}) => (primary ? "#101522" : "#cd853f")};
+  padding: ${({big}) => (big ? "16px 40px" : "14px 24px")};
+  color: ${({primary}) => (primary ? "#fff" : "#000d1a")};
+  font-size: ${({big}) => (big ? "20px" : "14px")};
   text-decoration: none;
   outline: none;
   border: none;
@@ -17,7 +17,7 @@ export const Button = styled(Link)`
   align-items: center;
   transition: all 0.2s ease-in-out;
 
-  &:hover{
+  &:hover {
     transform: translateY(-2px);
   }
-`
+`;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import styled, { css } from 'styled-components/macro';
-import { Link } from 'react-router-dom';
-import { menuData } from '../data/MenuData';
-import { Button } from './Button';
-import { FaBars } from 'react-icons/fa';
+import React, {useState} from "react";
+import styled, {css} from "styled-components/macro";
+import {Link} from "react-router-dom";
+import {menuData} from "../data/MenuData";
+import {Button} from "./Button";
+import {FaBars} from "react-icons/fa";
 
 const Nav = styled.nav`
   height: 60px;
@@ -13,7 +13,14 @@ const Nav = styled.nav`
   z-index: 100;
   position: fixed;
   width: 100%;
-`
+  background: transparent;
+  transition: 0.5s;
+  transition-timing-function: ease-in-out;
+
+  &.active {
+    background: #101522;
+  }
+`;
 
 const NavLink = css`
   color: #fff;
@@ -23,17 +30,18 @@ const NavLink = css`
   height: 100%;
   cursor: pointer;
   text-decoration: none;
-`
+`;
 
 const Logo = styled(Link)`
   ${NavLink}
+  font-size: 2rem;
   font-style: italic;
-`
+`;
 
 const MenuBars = styled.div`
   display: none;
 
-  @media screen and (max-width: 768px){
+  @media screen and (max-width: 768px) {
     color: #fff;
     display: block;
     font-size: 2.5rem;
@@ -43,35 +51,47 @@ const MenuBars = styled.div`
     right: 10px;
     transform: translateY(-50%, 25%);
   }
-`
+`;
 
 const NavMenu = styled.div`
   display: flex;
-  align-items: center ;
+  align-items: center;
   margin-right: -48px;
 
-  @media screen and (max-width: 768px){
+  @media screen and (max-width: 768px) {
     display: none;
   }
-`
+`;
 
 const NavMenuLinks = styled(Link)`
   ${NavLink}
-`
+`;
 
 const NavBtn = styled.div`
   display: flex;
   align-items: center;
   margin-right: 24px;
 
-  @media screen and (max-width: 768px){
+  @media screen and (max-width: 768px) {
     display: none;
   }
-`
+`;
 
 function Navbar({toggle}) {
+  const [navBackActive, setNavBackActive] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 800) {
+      setNavBackActive(true);
+    } else {
+      setNavBackActive(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeBackground);
+
   return (
-    <Nav>
+    <Nav className={navBackActive ? "active" : ""}>
       <Logo to="/">ELIXR</Logo>
       <MenuBars onClick={toggle}>
         <FaBars />
@@ -84,7 +104,7 @@ function Navbar({toggle}) {
         ))}
       </NavMenu>
       <NavBtn>
-        <Button to='/contact' primary='true'>
+        <Button to="/contact" primary="false">
           Contact Us
         </Button>
       </NavBtn>
